576f86a72e2d1a267b8bb71dd17ec82ea97d3118
Kun yang <claire2009@users.noreply.github.com>
2015-02-16 21:38:48 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of functions that computes the inverse of a matrix and store it in the cache. 
+## It first checks to see if the inverse of the matrix has already been computed. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and sets it in the cache.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The function "makeCacheMatrix" creates a special "matrix" object that can cache its inverse.
 
+makeCacheMatrix <- function(x = matrix()) { 
+  ## create a special "matrix object"
+  m <- NULL
+  ## set the value of the matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ## get the value of the matrix
+  get <- function() x
+  ## set the value of the inverse matrix
+  setinverse <- function(solve) m <<- solve
+  ## get the value of the inverse matrix
+  getinverse <- function(solve) m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function "cacheSolve" computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated, then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## check if the inverse of the matrix has already been calculated. If so, retrive the inverse from the cache.
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ## If the inverse has not been calculated yet, compute it now and set it in the cache.
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
